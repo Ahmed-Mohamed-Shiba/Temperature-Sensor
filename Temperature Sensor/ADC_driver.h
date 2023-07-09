f89308ee77efd978d716943e926bba7db2e83515
Ahmed-Mohamed-Shiba <121357837+Ahmed-Mohamed-Shiba@users.noreply.github.com>
2023-07-09 22:30:45 +0300

    Add files via upload

---
+++ Temperature Sensor/ADC_driver.h
@@ -0,0 +1,16 @@
+/*
+ * ADC_driver.h
+ *
+ *  Created on: Jul 10, 2022
+ *      Author: Ahmed Shiba
+ */ 
+
+
+#ifndef ADC_DRIVER_H_
+#define ADC_DRIVER_H_
+void ADC_vinit(void);
+ unsigned short ADC_u16Read(void);
+
+
+
+#endif /* ADC_DRIVER_H_ */
---
+++ Temperature Sensor/LCD_config.h
@@ -0,0 +1,10 @@
+/*
+ * LCD_config.h
+ *
+ *  Created on: Jul 6, 2022
+ *      Author: Ahmed Shiba
+ */
+#ifndef LCD_CONFIG_H_
+#define LCD_CONFIG_H_
+#define four_bits_mode
+#endif /* LCD_CONFIG_H_ */
